b5a97685d0ed548ecf142aca50d37bc64c9d79f4
AalaNC <AalaN1611@gmail.com>
2022-10-26 14:03:00 -0300

    Creacion de Componentes

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { RedesComponent } from './components/redes/redes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    RedesComponent
   ],
   imports: [
     BrowserModule,
